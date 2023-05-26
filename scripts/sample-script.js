c6674775e5553d6c4cf411e69262dd76b9092623
Jayachandra777 <124385565+Jayachandra777@users.noreply.github.com>
2023-05-26 12:21:27 +0530

    Add files via upload

---
+++ scripts/sample-script.js
@@ -0,0 +1,12 @@
+async function main() {
+    const CrowdsourcedFunding = await ethers.getContractFactory("CrowdsourcedFunding");
+    const crowdsourcedFunding = await CrowdsourcedFunding.deploy();
+  
+    console.log("Contract deployed to address:", crowdsourcedFunding.address);
+  }
+  
+  main().then(() => process.exit(0)).catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+  
